1304a3dce626cea5bfe42e6201894426609f9003
andyblueyo <abchen@uw.edu>
2017-04-11 03:38:28 -0700

    rename cols

--- server.R
+++ server.R
@@ -1,8 +1,19 @@
 library(shiny)
 grammar <- read.csv(file = "comma-survey-data.csv", stringsAsFactors = FALSE)
+print(colnames(grammar))
+colnames(grammar) <- c("Respondent ID", "In your opinion which sentence is more gramatically correct?", 
+                       "Prior to reading about it above, had you heard of the serial or Oxford comma?", 
+                       "How much if at all do you care about the use or lack thereof of the serial or Oxford comma in grammar?",
+                       "How would you write the following sentence?", 
+                       "When faced with using the word data have you ever spent time considering if the word was a singular or plural noun?",
+                       "How much if at all do you care about the debate over the use of the word data as a singluar or plural noun?",
+                       "In your opinion how important or unimportant is proper use of grammar?",
+                       "Gender", "Age", "Household Income", "Education", "Location Census Region")
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  output$view <- renderTable({
+    grammar
+  })
   
 })
--- ui.R
+++ ui.R
@@ -3,7 +3,11 @@ grammar <- read.csv(file = "comma-survey-data.csv", stringsAsFactors = FALSE)
 
 shinyUI(fluidPage(
   
-  titlePanel("The data of Grammar")
+  titlePanel("Grammar"),
+  
+  mainPanel(
+    tableOutput("view")
+  )
   
   
 ))
