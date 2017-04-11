a0e060cfd59bcdc68a427d01a51cfadf5513b208
andyblueyo <abchen@uw.edu>
2017-04-11 01:25:35 -0700

    read in csv

---
+++ server.R
@@ -0,0 +1,8 @@
+library(shiny)
+grammar <- read.csv(file = "comma-survey-data.csv", stringsAsFactors = FALSE)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,9 @@
+library(shiny)
+grammar <- read.csv(file = "comma-survey-data.csv", stringsAsFactors = FALSE)
+
+shinyUI(fluidPage(
+  
+  titlePanel("The data of Grammar")
+  
+  
+))
