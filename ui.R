ee93bbc235f1b4a9faceadf78a497519851674d2
andyblueyo <abchen@uw.edu>
2017-04-12 14:06:35 -0700

    recode blank entries

--- ui.R
+++ ui.R
@@ -1,13 +1,32 @@
 library(shiny)
+library(plotly)
+
 grammar <- read.csv(file = "comma-survey-data.csv", stringsAsFactors = FALSE)
 
 shinyUI(fluidPage(
   
-  titlePanel("Grammar"),
+  titlePanel("Grammar Trends"),
+  p("This shiny application is built to answer questions about grammar usage in terms of demographic and
+      prefernces of grammar practices such as the oxford comma. This dataset is from fivethirtyeight."),
+  
+  sidebarLayout(
+    sidebarPanel(
+    checkboxGroupInput("checkage", label = h3("checkbox"), choices = unique(grammar$Age), selected = "30-44")),
+    mainPanel(
+      titlePanel("Title"),
+      plotOutput("viewPlot")
+    )
+  ),
   
   mainPanel(
-    tableOutput("view")
+    headerPanel("Table of Grammar"),
+    plotOutput("viewPlot")
+  ),
+  mainPanel(
+
   )
+  
+ 
   
   
 ))
